a6b022fbd63353abbf3654dbc7817921e3219b40
pragatheeswaranpragi <pragatheeswaran@techfully.io>
2022-06-12 08:34:39 +0530

    banner added

---
+++ pages/component/banner.jsx
@@ -0,0 +1,41 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import Link from 'next/link';
+
+
+export default function Example() {
+return (
+<div>
+    <section class="text-white bg-gray-900">
+        <main className="font-mono relative overflow-hidden py-24">
+            <div className="flex relative z-20 items-center">
+                <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
+                    <div className="flex flex-col">
+                        <img src="/img/pragi.jpg" className="rounded-full w-28 mx-auto" />
+                        <div class="max-w-3xl mx-auto text-center pt-8">
+                            <h1
+                                className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+                                Pragatheeswaran
+                                <span className="sm:block">
+                                    Front-End Developer
+                                </span>
+                            </h1>
+                        </div>
+                        <div className='mx-auto py-8 cursor-pointer'>
+                            <Link href="mailto:pragatheespragi98@gmail.com">
+                            <div className="flex items-center justify-center mt-4">
+                                <div className='flex items-center'>
+                                    <img src="/img/logo-google.svg" className='w-8 fill-stone-600' />
+                                    <p className='pl-5'>pragatheespragi98@gmail.com</p>
+                                </div>
+                            </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </section>
+</div>
+)
+}
